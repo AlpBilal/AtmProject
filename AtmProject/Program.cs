ff4f9a45be3c1f0cc4dfd13d08c2d5d0b616a3ba
Alp <balpbilal@gmail.com>
2026-08-20 11:40:31 +0300

    Qr code implemented

--- AtmProject/Program.cs
+++ AtmProject/Program.cs
@@ -1,22 +1,84 @@
 ﻿using AtmProject;
 using System.IO;
-using ZXing;
+using ZXing.Windows.Compatibility;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 List<User> users = [
     new User("Alp", 5, "abcd", 4524, 100.24M), 
     new ("bh", 4, "bscd", 4526, 100.25M),
-    new ("bh", 4, "bscd", 4526, 100.25M)
+    new ("dh", 3, "bscd", 4524, 100.25M)
     ];
 List<Transaction> transactions = new List<Transaction>();
 bool open = true;
+
     while (open)
     {
+        Console.WriteLine("How would you like to login...\n1. name and PIN\n2.qrcode scan\n3.get a qrcode");
+        var choice = Convert.ToInt32(Console.ReadLine());
+        var name = string.Empty;
+        var PIN = 0;
+        User activeUser = null;
+        
+        switch (choice){
+        case 1:
+        Console.WriteLine("Your name: ");
+        name= Console.ReadLine();
+        Console.WriteLine("Your PIN: ");
+        PIN = Convert.ToInt32(Console.ReadLine());
+        activeUser = users.FirstOrDefault(u => u.Username == name && u.PIN == PIN);
+        break;
+        case 2:{
+        var reader = new BarcodeReader();
+        using var barcodeBitmap = (Bitmap)Image.FromFile(@"c:\Users\Alpbilal.basar\Downloads\username-qr.png");
+        var decode = reader.Decode(barcodeBitmap);
+        
+        if (decode != null)
+                {
+                    
+                    var RealUser = users.FirstOrDefault(u => u.Username == decode.Text);
+                    if(RealUser != null){
+                        activeUser = RealUser;
+                        name = decode.Text;
+                        Console.WriteLine("Congrats you logged in");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not a user");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No QR code could be read.");
+                }
+        }
+        break;
+        case 3:
+        Console.WriteLine("Your name: ");
+        name= Console.ReadLine();
+        var realUser = users.FirstOrDefault(u => u.Username == name);
+        if (realUser != null)
+            {
+                var writer = new BarcodeWriter
+                {
+                    Format = ZXing.BarcodeFormat.QR_CODE
+                };
+                var image = writer.Write(name);
+                var filePath = @"c:\Users\Alpbilal.basar\Downloads\username-qr.png";
+                image.Save(filePath, ImageFormat.Png);
+                Console.WriteLine($"QR code saved to {filePath}");
+            }
+        else
+            {
+                Console.WriteLine("You are not a real user");
+            }
 
-    Console.WriteLine("Your name: ");
-    var name= Console.ReadLine();
-    Console.WriteLine("Your PIN: ");
-    var PIN = Convert.ToInt32(Console.ReadLine());
-    User activeUser = users.FirstOrDefault(u => u.Username == name && u.PIN == PIN);
+        break;
+        default:
+        Console.WriteLine("Not a valid input... Try again");
+        break;
+    }
+    
 
     if (activeUser != null)
     {
